c1fd954c703257810c7fc7a17a7d883aedba1033
ryoudra <hritikrudra420@gmail.com>
2026-01-04 16:47:32 +0530

    synchronization issue without locks

---
+++ src/SynchronizationIssue/AdderSubtractorLocks/Adder.java
@@ -0,0 +1,31 @@
+package SynchronizationIssue.AdderSubtractorLocks;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.locks.Lock;
+
+public class Adder implements Callable<Void> {
+
+    private Value value;
+    private Lock lock;
+
+    public Adder(Value value, Lock lock) {
+        this.value = value;
+        this.lock = lock;
+    }
+
+
+    /**
+     * Computes a result, or throws an exception if unable to do so.
+     *
+     * @return computed result
+     * @throws Exception if unable to compute a result
+     */
+    @Override
+    public Void call() throws Exception {
+
+        for (int i = 0; i < 10000; i++) {
+            this.value.setX(this.value.getX() + 1);
+        }
+return  null;
+    }
+}
---
+++ src/SynchronizationIssue/AdderSubtractorLocks/Client.java
@@ -0,0 +1,28 @@
+package SynchronizationIssue.AdderSubtractorLocks;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Client {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+        Value value = new Value();
+        Lock lock = new ReentrantLock();
+
+        Adder adder= new Adder(value, lock);
+        Subtractor subtractor = new Subtractor(value, lock);
+
+        ExecutorService executorService= Executors.newFixedThreadPool(2);
+        Future<Void> adderFuture= executorService.submit(adder);
+        Future<Void> subtractorFuture= executorService.submit(subtractor);
+
+        adderFuture.get();
+        subtractorFuture.get();
+
+        System.out.println(value.getX());
+    }
+}
